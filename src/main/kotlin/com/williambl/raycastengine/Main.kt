963a43079655c9cf73ad3cf32e37465358a98444
williambl <william@williambl.com>
2019-08-15 13:50:13 +0100

    Move init and loop into separate functions

--- src/main/kotlin/com/williambl/raycastengine/Main.kt
+++ src/main/kotlin/com/williambl/raycastengine/Main.kt
@@ -9,6 +9,9 @@ import org.lwjgl.system.MemoryUtil.NULL
 object Main {
 
     var window: Long = 0
+    var windowHeight: Int = 480
+    var windowWidth: Int = 640
+    var windowTitle: String = "Raycaster"
     var world = World(arrayOf(
             arrayOf(1, 2, 2, 2, 2, 2, 2, 2, 2, 1),
             arrayOf(1, 0, 0, 0, 0, 0, 0, 0, 0, 1),
@@ -27,7 +30,22 @@ object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        init()
+
+        while (true) {
+            loop()
+        }
+    }
+
+    fun init() {
+        initGLFW()
+        initOGL()
+
+        player.initKeyCallbacks()
+    }
+
+    fun initGLFW() {
+        //Initialize GLFW. Most GLFW functions will not work before doing this.
         if (!glfwInit()) {
             throw IllegalStateException("Unable to initialize GLFW")
         }
@@ -37,7 +55,7 @@ object Main {
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE)
 
-        window = glfwCreateWindow(640, 480, "Hello World!", NULL, NULL)
+        window = glfwCreateWindow(windowWidth, windowHeight, windowTitle, NULL, NULL)
         if (window == NULL) {
             throw RuntimeException("Failed to create the GLFW window")
         }
@@ -49,36 +67,33 @@ object Main {
 
         // Make the window visible
         glfwShowWindow(window)
+    }
 
+    fun initOGL() {
         GL.createCapabilities()
         // Set the clear color
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f)
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        glOrtho(0.0, 640.0, 0.0, 480.0, 1.0, -1.0)
+        glOrtho(0.0, windowWidth.toDouble(), 0.0, windowHeight.toDouble(), 1.0, -1.0)
         glMatrixMode(GL_MODELVIEW)
-
-        player.initKeyCallbacks()
-
-        val loop = fun () {
-            // Clear the framebuffer
-            glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
-
-            // Poll for window events. The key callback above will only be
-            // invoked during this call.
-            glfwPollEvents()
-
-            // Render the world
-            renderer.render(world, player)
-
-            println(player.x.toString() + ", " + player.y.toString() + ", " + player.dir.toString())
-
-            // Swap the color buffers
-            glfwSwapBuffers(window)
-        }
-
-        while (true) {
-            loop()
-        }
     }
+
+    fun loop() {
+        // Clear the framebuffer
+        glClear(GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT)
+
+        // Poll for window events. The key callback above will only be
+        // invoked during this call.
+        glfwPollEvents()
+
+        // Render the world
+        renderer.render(world, player)
+
+        println(player.x.toString() + ", " + player.y.toString() + ", " + player.dir.toString())
+
+        // Swap the color buffers
+        glfwSwapBuffers(window)
+    }
+
 }
