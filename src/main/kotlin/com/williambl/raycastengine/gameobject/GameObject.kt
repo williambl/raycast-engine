2bae4459e646df510a0f7326146ace3a6d5185a2
williambl <william@williambl.com>
2020-07-07 18:07:59 +0100

    Give all gameobjects uuids

--- src/main/kotlin/com/williambl/raycastengine/gameobject/GameObject.kt
+++ src/main/kotlin/com/williambl/raycastengine/gameobject/GameObject.kt
@@ -1,8 +1,11 @@
 package com.williambl.raycastengine.gameobject
 
 import com.williambl.raycastengine.world.World
+import java.util.*
 
 open class GameObject(var x: Double, var y: Double) {
 
     lateinit var world: World
+
+    open var id: UUID = UUID.randomUUID()
 }
