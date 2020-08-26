9ba780d8af1756e39ddbd73583fc6ee77e9da2fe
williambl <william@williambl.com>
2020-08-26 19:49:38 +0100

    Add example GUI

--- src/main/kotlin/com/williambl/raycastengine/gameobject/InteractableSprite.kt
+++ src/main/kotlin/com/williambl/raycastengine/gameobject/InteractableSprite.kt
@@ -1,19 +1,41 @@
 package com.williambl.raycastengine.gameobject
 
+import com.beust.klaxon.JsonObject
+import com.williambl.raycastengine.Main
 import com.williambl.raycastengine.collision.AxisAlignedBoundingBox
-import com.williambl.raycastengine.util.network.readGameObject
+import com.williambl.raycastengine.render.gui.Gui
 import com.williambl.raycastengine.util.network.writeGameObject
 import com.williambl.raycastengine.util.raytrace.RaytraceResult
+import imgui.ImGui
 import io.netty.buffer.Unpooled
-import java.nio.charset.Charset
 
 class InteractableSprite(textureLoc: String = "", x: Double = 0.0, y: Double = 0.0) : Sprite(textureLoc, x, y), Interactable, Collidable {
+
+    val gui = MyGui()
+
+    inner class MyGui : Gui {
+        var message = ""
+        var shouldBeOpen = false
+
+        override fun renderGui(): ImGui.() -> Any? = {
+            if (shouldBeOpen) {
+                begin("Hi there!", ::shouldBeOpen)
+                text("As JSON, I am:")
+                textWrapped(message)
+                end()
+            }
+        }
+    }
+
+    init {
+        Main.renderTickables.add(gui)
+    }
+
     override fun interact(player: Player, raytraceResult: RaytraceResult<*>) {
-        val buf = Unpooled.buffer()
-        buf.writeGameObject(this)
-        println(buf.toString(0, buf.writerIndex() + 1, Charset.defaultCharset()))
-        world.removeGameObject(this)
-        world.addGameObject(buf.readGameObject())
+        gui.shouldBeOpen = true
+        val json = JsonObject()
+        toJson(json)
+        gui.message = json.toJsonString(prettyPrint = true)
     }
 
     override fun getAABB() = AxisAlignedBoundingBox(x - 0.5, y - 0.5, x + 0.5, y + 0.5, this)
--- src/main/kotlin/com/williambl/raycastengine/render/gui/Gui.kt
+++ src/main/kotlin/com/williambl/raycastengine/render/gui/Gui.kt
@@ -1,13 +1,9 @@
 package com.williambl.raycastengine.render.gui
 
-import com.williambl.raycastengine.Main
-import com.williambl.raycastengine.Main.implGl3
-import com.williambl.raycastengine.Main.implGlfw
 import com.williambl.raycastengine.events.RenderTickable
 import imgui.ImGui
 
 interface Gui: RenderTickable {
-
     override fun renderTick() {
         ImGui.run(renderGui())
     }
