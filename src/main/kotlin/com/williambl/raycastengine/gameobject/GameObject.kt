2a593c4f1b62e6ef0bd553f932cac84dcb1fc9f7
williambl <william@williambl.com>
2021-01-30 01:00:39 +0000

    Give GameObject some KDoc

--- src/main/kotlin/com/williambl/raycastengine/gameobject/GameObject.kt
+++ src/main/kotlin/com/williambl/raycastengine/gameobject/GameObject.kt
@@ -8,6 +8,22 @@ import com.williambl.raycastengine.world.World
 import io.netty.buffer.ByteBuf
 import java.util.*
 
+/**
+ * An object in the game world.
+ * This is similar to an 'entity' in Minecraft or a 'GameObject' in Unity.
+ *
+ * A GameObject is added to the world with [World.addGameObject]
+ * and removed from the world with [World.removeGameObject]
+ * (Note that this is not currently synced between players)
+ *
+ * Making GameObjects multiplayer-safe requires some work:
+ *
+ *  - To have a property auto-synced across from server->client on change, use the [synced] delegate.
+ *  - Any properties that must be synced from server->client on initial connect **must** be handled in [toBytes] and [fromBytes]
+ *  - (This includes auto-synced properties)
+ *  - **All constructor parameters must have a default value**. (If you're writing in Java, your GameObject must have a 0-arg constructor).
+ *  This is because on the client, GameObjects are first created with a 0-arg constructor and their data is given to them with [fromBytes].
+ */
 open class GameObject(xIn: Double = 0.0, yIn: Double = 0.0) {
 
     var x: Double by synced(xIn, ::id, ByteBuf::writeDouble, ByteBuf::readDouble)
